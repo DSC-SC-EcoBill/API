021de69f661fbebba22ff697ececd99d4fab895c
ksanbal <dev.ksanbal@gmail.com>
2020-03-05 22:45:41 +0900

    Django 프로젝트를 생성했습니다.

---
+++ API/EReceipt/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EreceiptConfig(AppConfig):
+    name = 'EReceipt'
---
+++ API/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gimhyeongyun/Documents/Programing/DSC/SC/TrashIssue/API/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
