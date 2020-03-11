e5c627c574d7b49bc0356433273dde140ed8ef4f
sbsbbb <1213hope@naver.com>
2020-03-11 21:50:07 +0900

    모델 변경

--- API/EReceipt/models.py
+++ API/EReceipt/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 
 # Create your models here.
-class Users(models.Model):
+class Guests(models.Model):
     # 모든 테이블의 id(pk)는 자동적으로 auto_increment로 생성
-    user_id = models.CharField(max_length=16)
-    password = models.CharField(max_length=40)
+    guest_id = models.CharField(max_length=16)
+    guest_password = models.CharField(max_length=40)
     name = models.CharField(max_length=16)
     jumin_no = models.IntegerField(null=True)
-    email = models.EmailField(max_length=64)
+    email = models.EmailField(default="email@example.com")
 
 
 class Devices(models.Model):
+    device_id = models.CharField(max_length=16)
+    device_password = models.CharField(max_length=40)
     device_date = models.DateTimeField(auto_now_add=True)  # inseart한 현재시간 자동저장
 
 
@@ -24,16 +26,6 @@ class Receipt(models.Model):
 
 
 class Qrcodes(models.Model):
-    qrcode_img_url = models.CharField(max_length=1000)
+    qrcode_img_url = models.TextField()
     qrcode_date = models.DateTimeField(auto_now_add=True)
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-
-
-class Logs(models.Model):
-    receipt_img_url = models.CharField(max_length=1000)
-    log_generated_dt = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-    device = models.ForeignKey(Devices, on_delete=models.CASCADE)
-
-
