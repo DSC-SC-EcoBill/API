621c06b3c91beef843e6c865b4fc9bc64c1bbf17
ksanbal <dev.ksanbal@gmail.com>
2020-03-13 19:03:05 +0900

    admin 설정을 한국어로 바꾸고, superuser를 생성했는데 로그인이 안되...ㅎ

--- API/EReceipt/admin.py
+++ API/EReceipt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Guests)
+admin.site.register(Devices)
+admin.site.register(Receipt)
+admin.site.register(Qrcodes)
+
