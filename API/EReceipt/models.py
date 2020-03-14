badee53261540ed97aceb745521a4565297ec6b0
hope1213 <1213hope@gmail.com>
2020-03-15 02:08:36 +0900

    mysql로 데이터베이스 변경하고 mysql workbench랑 연결해서 슈퍼유저로으로 데이터 추가하는거 잘되나까지 했서여

--- API/EReceipt/models.py
+++ API/EReceipt/models.py
@@ -4,28 +4,27 @@ from django.db import models
 # Create your models here.
 class Guests(models.Model):
     # 모든 테이블의 id(pk)는 자동적으로 auto_increment로 생성
-    guest_id = models.CharField(max_length=16)
-    guest_password = models.CharField(max_length=40)
-    name = models.CharField(max_length=16)
+    guest_id = models.CharField(max_length=16, default='')
+    guest_password = models.CharField(max_length=40, default='')
+    name = models.CharField(max_length=16, default='')
     jumin_no = models.IntegerField(null=True)
     email = models.EmailField(default="email@example.com")
 
 
 class Devices(models.Model):
-    device_id = models.CharField(max_length=16)
-    device_password = models.CharField(max_length=40)
-    device_date = models.DateTimeField(auto_now_add=True)  # inseart한 현재시간 자동저장
+    device_date = models.DateTimeField(auto_now_add=True)
+    device_id = models.CharField(max_length=8, default='')
 
 
 class Receipt(models.Model):
-    receipt_img_url = models.TextField()
+    receipt_img_url = models.TextField(default='')
     receipt_date = models.DateTimeField(auto_now_add=True)
-    qr_url = models.TextField()
+    qr_url = models.TextField(default='')
     flag = models.IntegerField(default=0)
     user = models.ForeignKey(Guests, on_delete=models.CASCADE)
 
 
 class Qrcodes(models.Model):
-    qrcode_img_url = models.TextField()
+    qrcode_img_url = models.TextField(default='')
     qrcode_date = models.DateTimeField(auto_now_add=True)
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
