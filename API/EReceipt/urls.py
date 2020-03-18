56a6698a1e4bde40d55d565c7d95769b126d67bb
hope1213 <1213hope@gmail.com>
2020-03-18 22:51:42 +0900

    ^^

--- API/EReceipt/urls.py
+++ API/EReceipt/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('hello/', HelloAPI),
     path('auth/signup/', SignupAPI.as_view()),
     path('auth/signin/', SignIn),
-    path('main/upload_receipt/', UplaodReceipt.as_view()),
     path('main/rimg/', ReturnImg),
     path('main/rlist/', ReturnList),
     path('main/ritemimg/', ReturnItemImg),
