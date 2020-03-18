d571d01faff8563a08782e3dfeeef95ec395e887
ksanbal <dev.ksanbal@gmail.com>
2020-03-19 05:06:28 +0900

    이미지 올리는 url 수정하고, 개인화 하는 url 작성했어

--- API/EReceipt/urls.py
+++ API/EReceipt/urls.py
@@ -1,14 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('auth/signup/', SignupAPI.as_view()),
     path('auth/signin/', SigninAPI.as_view()),
 
-    path('main/gcptest/', Upload_Receipt),
+    # 디바이스 to API, 영수증 이미지 올리는 url
+    path('main/upload_img/', UploadIMG.as_view()),
+    path('main/check_user_link/<int:creat_receipt_id>', CheckUser.as_view()),
 
     path('main/return_receipt_img_List/', ReturnReceiptImgList.as_view()),
     path('main/receipt_url/', NewReceiptURL.as_view()),
     path('main/receipt_date/', ReceiptDate.as_view()),
-    path('main/imagecache/', ImageCache.as_view()),
 ]
