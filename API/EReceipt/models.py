9ccc1f53a40e04a73c6499991179e83ecfba2f2d
ksanbal <dev.ksanbal@gmail.com>
2020-03-19 05:04:59 +0900

    이미지 이름도 받을 수 있도록 모델에 열을 하나 추가했어

--- API/EReceipt/models.py
+++ API/EReceipt/models.py
@@ -20,4 +20,7 @@ class Qrcodes(models.Model):
 # 영수증 이미지를 잠시 저장하는 모델
 class ImageCache(models.Model):
     upload_data = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(default='media/default_image.jpg')
+    device_id = models.CharField(max_length=20, default='')
+    image_name = models.CharField(max_length=20, default='')
+    image = models.ImageField(default='default_img.jpg')
+
