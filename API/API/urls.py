4295d395aa13c71f122507d1e9abe945ad6046f3
ksanbal <dev.ksanbal@gmail.com>
2020-04-28 20:13:35 +0900

    url에 , 추가

--- API/API/urls.py
+++ API/API/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('EReceipt.urls')),
-    path('api/auth', include('knox.urls'))
+    path('api/auth', include('knox.urls')),
 ]
